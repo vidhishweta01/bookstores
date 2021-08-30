0fa7ce6947a3680e72942f24229bf871a94ff492
vidhishweta01 <vidhishweta01@gmail.com>
2021-08-31 02:17:08 +0530

    destructured prop in book.js removed eslint-disable comment added proptype

--- src/components/book/book.js
+++ src/components/book/book.js
@@ -1,13 +1,21 @@
-const Book = (book, key) => {
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/no-unused-prop-types */
+import propTypes from 'prop-types';
+
+const Book = (book) => {
   const { obj } = book;
   const { id, title, category } = obj;
   return (
     <tr>
-      <td id={key}>{id}</td>
+      <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
     </tr>
   );
+};
+
+Book.propTypes = {
+  book: propTypes.object.isRequired,
 };
 
 export default Book;
