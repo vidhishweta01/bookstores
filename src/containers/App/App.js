3bf6d20db6cacd0598644acff9ebba087f80c0f8
vidhishweta01 <vidhishweta01@gmail.com>
2021-09-03 18:44:55 +0530

    styled app added background color

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,8 +1,11 @@
 import BooksList from '../BooksList';
 import BooksForm from '../../components/BookForm';
+import Navbar from '../../components/navbar';
+import './App.css';
 
 const App = () => (
   <div>
+    <Navbar />
     <BooksList />
     <BooksForm />
   </div>
